5152f9782e7020a958e79040f8370eab5dfb9ed3
Jin Wu <jinn.wuu@gmail.com>
2020-02-21 12:14:49 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const createTaskForm = document.querySelector("#create-task-form")
+  createTaskForm.addEventListener("submit", newTask)
+
+})
+
+const newTask = event => {
+  event.preventDefault()
+  const taskDescription = document.querySelector("#new-task-description")
+  const taskElement = document.createElement("li")
+  taskElement.textContent = taskDescription.value
+
+  document.querySelector("#tasks").append(taskElement)
+}
